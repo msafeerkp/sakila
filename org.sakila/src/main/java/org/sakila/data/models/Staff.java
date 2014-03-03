4839fb582d2f774685c3e88606394af8cebcb70f
msafeer <msafeerkp@gmail.com>
2014-03-03 18:01:02 +0530

    Data Layer creation is in progress.
    Signed-off-by: msafeer <msafeerkp@gmail.com>

--- org.sakila/src/main/java/org/sakila/data/models/Staff.java
+++ org.sakila/src/main/java/org/sakila/data/models/Staff.java
@@ -1,9 +1,20 @@
 package org.sakila.data.models;
 
+import java.sql.Blob;
+import java.util.Date;
+
 public class Staff {
 	
-	private String staffId;
+	private long staffId;
 	private String firstName;
 	private String lastName;
+	private Blob picture;
+	private String email;
+	private long active;
+	private String userName;
+	private String password;
+	private Date lastUpdate;
+	
+	
 	
 }
